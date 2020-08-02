1b3ad6f5c67a0c85eb5ed2f5662a276218cc38f6
lgnstar <lgnstar@qq.com>
2020-08-02 19:01:26 +0800

    Signed-off-by: lgnstar <lgnstar@qq.com>

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include "stc8.h"
+
 #include "wifi.h"
+
 
 typedef struct
 {
-
+  unsigned char delay;
 }stKeyStatus;
 
 void hardware_init(void);
+
+void usart1_SendStr(unsigned char *str);
+void usart1_SendHexStr(unsigned char *pdat, unsigned char len);
+
 void usart4_SendByte(unsigned char dat);
 
 
@@ -15,9 +21,13 @@ void main()
   hardware_init();
   wifi_protocol_init();
 
+  usart1_SendStr("begin ................................\r\n");
+
   while(1)
   {
     wifi_uart_service();
+    usart1_SendStr("OK!\r\n");
+    P23 = !P23;
   }
 }
 
@@ -25,42 +35,42 @@ void hardware_init(void)
 {
   //初始化串口1, 115200bps@11.0592MHz, 输出log
   P_SW1 = 0x00;     //RXD/P3.0, TXD/P3.1
-  SCON = 0x50;		  //8位数据,可变波特率
-	AUXR |= 0x01;		  //串口1选择定时器2为波特率发生器
-	AUXR &= 0xFB;		  //定时器2时钟为Fosc/12,即12T
-	T2L = 0xFE;		    //设定定时初值
-	T2H = 0xFF;		    //设定定时初值
-	AUXR |= 0x10;		  //启动定时器2
-	IE |= 0x10;       //串行口1中断允许
+  SCON = 0x50;      //8位数据,可变波特率
+  AUXR |= 0x01;     //串口1选择定时器2为波特率发生器
+  AUXR &= 0xFB;     //定时器2时钟为Fosc/12,即12T
+  T2L = 0xFE;       //设定定时初值
+  T2H = 0xFF;       //设定定时初值
+  AUXR |= 0x10;     //启动定时器2
+  IE |= 0x10;       //串行口1中断允许
 
   //初始化串口4, 9600bps@11.0592MHz, 与wifi模块通信
   P_SW2 = 0x04;     //RXD4_2/P5.2, TXD4_2/P5.3
-  S4CON = 0x10;		  //8位数据,可变波特率
-	S4CON |= 0x40;		//串口4选择定时器4为波特率发生器
-	T4T3M &= 0xDF;		//定时器4时钟为Fosc/12,即12T
-	T4L = 0xE8;		    //设定定时初值
-	T4H = 0xFF;		    //设定定时初值
-	T4T3M |= 0x80;		//启动定时器4
+  S4CON = 0x10;     //8位数据,可变波特率
+  S4CON |= 0x40;    //串口4选择定时器4为波特率发生器
+  T4T3M &= 0xDF;    //定时器4时钟为Fosc/12,即12T
+  T4L = 0xE8;       //设定定时初值
+  T4H = 0xFF;       //设定定时初值
+  T4T3M |= 0x80;    //启动定时器4
   IE2 = 0x10;
 
   //定时计数器1, 25毫秒@11.0592MHz, 用于按键去抖动
-  AUXR &= 0xBF;		  //定时器时钟12T模式
-	TMOD &= 0x0F;		  //设置定时器模式
-	TL1 = 0x00;		    //设置定时初值
-	TH1 = 0xA6;		    //设置定时初值
-	TF1 = 0;		      //清除TF1标志
-	TR1 = 1;		      //定时器1开始计时
+  AUXR &= 0xBF;     //定时器时钟12T模式
+  TMOD &= 0x0F;     //设置定时器模式
+  TL1 = 0x00;       //设置定时初值
+  TH1 = 0xA6;       //设置定时初值
+  TF1 = 0;          //清除TF1标志
+  TR1 = 1;          //定时器1开始计时
   ET1 = 1;          //定时计数器T1溢出中断允许
 
   EA = 1;
 }
 
-void usart1_SendByte(uint8_t dat)
+static void usart1_SendByte(unsigned char dat)
 {
   SBUF = dat;  while(!TI);  TI = 0;
 }
 
-void usart1_SendStr(uint8_t *str)
+void usart1_SendStr(unsigned char *str)
 {
   while(*str)
   {
@@ -68,9 +78,9 @@ void usart1_SendStr(uint8_t *str)
   }
 }
 
-void usart1_SendHexStr(uint8_t *pdat, uint8_t len)
+void usart1_SendHexStr(unsigned char *pdat, unsigned char len)
 {
-  uint8_t data half, dest;
+  unsigned char data half, dest;
   while(len--)
   {
     half = *pdat >> 4;    half += 0x30;
@@ -104,7 +114,9 @@ void usart4_ISR(void) interrupt 18
 
 void timer1_ISR(void) interrupt 3
 {
-	TL1 = 0x00;	 TH1 = 0xA6;  TR1 = 1;
+  //TL1 = 0x00;   TH1 = 0xA6;  TR1 = 1;
+
+  P21 = !P21;
 }
 
 
